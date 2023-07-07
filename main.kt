4f084b1818e6f59cb18256d281ec1fc5420c4a74
Rodrigo Lopes <52975315+rodrigoczlopes@users.noreply.github.com>
2023-07-07 14:05:07 -0300

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,31 @@
+/**
+2
+ * You can edit, run, and share this code.
+3
+ * play.kotlinlang.org
+4
+ */
+5
+fun main() {
+6
+    println("Bem vindo ao Bytebank!")
+7
+    val titular: String = "Rodrigo"
+8
+    val numeroConta: Int = 1000
+9
+    var saldo = 0.0
+10
+    saldo = 100 + 2.0
+11
+    saldo += 200
+12
+    
+13
+    println("titular $titular")
+14
+    println("número da conta $numeroConta")
+15
+    println("saldo da conta $saldo")
+16
+}
